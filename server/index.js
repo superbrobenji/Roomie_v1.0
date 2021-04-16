a6739719a2cddc64896a3566ab9f7f93229fc0b4
superbrobenji <bennie.h.swanepoel@gmail.com>
2021-04-17 01:16:20 +0200

    removed scripts

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'test' });
+    res.send({ hi: 'dev' });
 });
 
 const PORT = process.env.PORT || 5000;
