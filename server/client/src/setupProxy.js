3e1d018aed19a77a1a420643118baab47a0b06bf
superbrobenji <bennie.h.swanepoel@gmail.com>
2021-04-24 23:46:27 +0200

    added react front end

---
+++ server/client/src/setupProxy.js
@@ -0,0 +1,9 @@
+import { createProxyMiddleware } from "http-proxy-middleware";
+module.exports =  (app) => {
+  app.use(
+    ["/api", "/auth/google"],
+    createProxyMiddleware({
+      target: "http://localhost:5000",
+    })
+  );
+};
