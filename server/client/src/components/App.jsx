071c314c3e7ed6902a8f7516dd8d4adace7f1053
superbrobenji <bennie.h.swanepoel@gmail.com>
2021-05-18 01:09:27 +0200

    added react routher. TODO: add actions and components as well as add auth page. also need to create the HOC

--- server/client/src/components/App.jsx
+++ server/client/src/components/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Landing = () => <div>Landing</div>
+const header = () => <div>Header</div>
+}
 
 const App = () => {
-	return <div>hello world</div>;
+	return (
+		<React.StrictMode>
+			<BrowserRouter>
+				<div>
+					<Header />
+					<Route exact path='/' compoennt={Landing} />
+				</div>
+			</BrowserRouter>
+		</React.StrictMode>
+	);
 };
 
 export default App;
